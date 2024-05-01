05c363cd65cae0df3d1364175e7d5cbaa73867bf
Sylla <ismailasyllaa@gmail.com>
2024-05-02 03:46:12 +0400

    Fix: route params

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Listing from './pages/ListingPage';
-import Details from './pages/DetailsPage';
+import ListingPage from './pages/ListingPage';
+import DetailsPage from './pages/DetailsPage';
+
 const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Listing />} />
-      </Routes>
-      <Routes>
-        <Route path="/details" element={<Details />} />
+        <Route path="/" element={<ListingPage />} />
+        <Route path="/details/:itemId" element={<DetailsPage />} />
       </Routes>
     </div>
   );
